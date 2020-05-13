b0434fc4c36e1e134b84cc3af37d7834af031073
Faber Vergara <fabervergara@gmail.com>
2020-05-12 21:49:32 -0500

    add prom by trimester

--- js/app.js
+++ js/app.js
@@ -11,6 +11,21 @@ let app = new Vue({
     axios.get('js/data.json')
       .then(function (response) {
         self.data = response.data
+
+        self.addSumAndProm()
       })
+  },
+  methods: {
+    addSumAndProm: function () {
+      this.data[this.activeID].trimester.forEach(item => {
+        // console.log("item", item)
+        item.prom = (item.mat + item.comp + item.eco + item.ing) / 4
+      });
+    }
+  },
+  watch: {
+    activeID: function () {
+      this.addSumAndProm()
+    }
   }
 })
